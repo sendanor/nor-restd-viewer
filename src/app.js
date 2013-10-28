5a1ea879fcd8fee4050585115419e55f1288e586
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-10-28 10:33:49 +0200

    Updated

--- src/app.js
+++ src/app.js
@@ -1,31 +1,64 @@
-/**
- * Module dependencies.
- */
+/** API viewer for Sendanor REST Daemon */
+module.exports = function(opts) {
+	opts = opts || {};
 
-var express = require('express');
-var path = require('path');
-var app = module.exports = express();
+	if(opts.url) {
+		opts.url = require('url').parse(opts.url);
+	}
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'hjs');
-app.use(express.favicon());
-//app.use(express.logger('dev'));
-//app.use(express.bodyParser());
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+	var util = require('util');
+	var express = require('express');
+	var path = require('path');
+	var app = module.exports = express();
+	
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'hjs');
 
-app.use(function(req, res) {
-	res.render('index', { title: 'Express' });
-});
+	Object.keys(opts).forEach(function(key) {
+		app.set('plugin ' + key, opts[key]);
+	});
+	
+	app.locals({'plugin': opts});
 
-//app.use(app.router);
+	app.use(express.favicon());
+	//app.use(express.logger('dev'));
+	//app.use(express.bodyParser());
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	
+	app.use(app.router);
 
-app.use(express.static(path.join(__dirname, 'public')));
+	/** Removes temporarily `from_path` from `req.url` */
+	/*
+	function strip_url_path(from_path, mod) {
+		function wrapper(req, res, next) {
 
-// development only
-if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
-}
+			var orig_url = req.url;
+			util.debug('req.url (before) = ' + util.inspect(req.url));
+			function do_next() {
+				req.url = orig_url;
+				return next.apply(undefined, Array.prototype.slice.call(arguments) );
+			}
+			req.url = req.url.substr( from_path.length );
+			util.debug('req.url (after) = ' + util.inspect(req.url));
+			return mod(req, res, do_next);
+		}
+		return wrapper;
+	}
+	*/
+	
+	app.use('/public', express.static(path.join(__dirname, 'public')) );
 
+	// development only
+	if ('development' === app.get('env')) {
+		app.use(express.errorHandler());
+	}
+	
+	app.use(function(req, res) {
+		res.render('index', { title: 'Express' });
+	});
+	
+	return app;
+};
 /* EOF */
